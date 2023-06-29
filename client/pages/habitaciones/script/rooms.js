aa7081364bbd24a9a7380f9e67c0683c5876eee5
Luciano Sanchez <cbrook3n@gmail.com>
2023-06-29 07:22:46 -0300

    first commit

---
+++ client/pages/habitaciones/script/rooms.js
@@ -0,0 +1,34 @@
+const roomContainer = document.getElementById("roomContainer");
+
+const rooms = [
+  {
+    name: "Habitacion Standard",
+    price: "$100",
+    image: "../../resources/images/standard.jpg",
+  },
+  {
+    name: "Habitacion Deluxe",
+    price: "$200",
+    image: "../../resources/images/deluxe.jpg",
+  },
+  {
+    name: "Suite",
+    price: "$300",
+    image: "../../resources/images/suite.jpg",
+  },
+];
+
+const roomCards = rooms.map((room) => {
+  return `
+    <div class="card">
+      <img src="${room.image}" class="card-img-top" alt="${room.name}" />
+      <div class="card-body">
+        <h5 class="card-title">${room.name}</h5>
+        <p class="card-text">Price: ${room.price}</p>
+        <a href="#" class="btn btn-primary">Book Now</a>
+      </div>
+    </div>
+  `;
+});
+
+roomContainer.innerHTML = roomCards.join("");
